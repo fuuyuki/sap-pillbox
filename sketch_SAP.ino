bc042908f98f4454449414b0b91cb19fe9aea7f4
Muhammad Fikra Adzaky <fikraadzaky123@gmail.com>
2026-04-18 14:14:33 +0700

    add chipId for SAP01-04

--- sketch_SAP.ino
+++ sketch_SAP.ino
@@ -7,9 +7,11 @@
 #include "74hc595.h"
 
 String apiKey = "b6d1c1cda37e9e8bd4d3730d8a20f51882c5f97075b2cdc18c683f320a3172bb";
-String chipIdStr = "C87BC4286F24";
-
-Schedule schedules[100];
+String chipIdStr = "F4A2B6B24354"; // SAP 01
+// String chipIdStr = "C87BC4286F24"; // SAP 02
+// String chipIdStr = "F0B5AD286F24"; // SAP 03
+// String chipIdStr = "00DE3855B594"; // SAP 04 bulge dibagian tutup bawah depan
+Schedule schedules[10];
 int scheduleCount = 0;
 
 unsigned long lastHeartbeat = 0;
@@ -28,6 +30,7 @@ const unsigned long scheduleRefreshInterval = 900000; // 15 minutes
 void setup() {
   Serial.begin(115200);
 
+  Wire.begin(SDA_PIN, SCL_PIN);
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println("SSD1306 allocation failed");
     for(;;); // halt
@@ -56,15 +59,16 @@ void loop() {
   }
 
   // Pill check interval
-  // if (currentMillis - lastCheck >= checkInterval) {
-  //   checkPillTimes();
-  //   lastCheck = currentMillis;
-  // }
+  if (currentMillis - lastCheck >= checkInterval) {
+    checkPillTimes();
+    lastCheck = currentMillis;
+  }
 
   // OLED time update interval
   if (currentMillis - lastOledUpdate >= oledInterval) {
     checkPillTimes();
     struct tm now;
+    
     if (getLocalTime(&now)) {
       oledShowTime(now, "Online"); // status can be "Online", "Offline", etc.
     }
