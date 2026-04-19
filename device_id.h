b456c94597dd6ef97fe2fc887407d33dafe4c633
Muhammad Fikra Adzaky <fikraadzaky123@gmail.com>
2026-04-19 07:25:46 +0700

    add apikey for each

--- device_id.h
+++ device_id.h
@@ -7,19 +7,21 @@
 struct DeviceMap {
   const char* id;
   const char* name;
+  const char* key;
 };
 
 // Your known devices
 static DeviceMap devices[] = {
-  {"F4A2B6B24354", "SAP01"},
-  {"C87BC4286F24", "SAP02"},
-  {"F0B5AD286F24", "SAP03"},
-  {"00DE3855B594", "SAP 04"}
+  {"F4A2B6B24354", "SAP01", "7cc30933c7b4ad980886fda2e58f1e01539d30620449bd05e742c6c1d2fffd04"},
+  {"C87BC4286F24", "SAP02", "b6d1c1cda37e9e8bd4d3730d8a20f51882c5f97075b2cdc18c683f320a3172bb"},
+  {"F0B5AD286F24", "SAP03", "08f5d1f653b0b65ef3b4600e74890fcc06b1130733023142e0658ee9a338bb8c"},
+  {"00DE3855B594", "SAP04", "71d3880b72db1e18f23cec342897fb155b25de1876990f08e6e1c173a53ef754"}
 };
 
 // Global variables
 String chipIdStr;
 String deviceName;
+String apiKey;
 
 // ---------- Helpers ----------
 String getChipIdStr() {
@@ -40,9 +42,19 @@ String deviceNameFromId(const String& chipId) {
   return "Unknown"; // fallback
 }
 
+String apiKeyFromId(const String& chipId) {
+  for (auto &d : devices) {
+    if (chipId.equalsIgnoreCase(d.id)) {
+      return String(d.key);
+    }
+  }
+  return "Unknown"; // fallback
+}
+
 // ---------- Initialization ----------
 void initDeviceId() {
   chipIdStr = getChipIdStr();
+  apiKey = apiKeyFromId(chipIdStr);
   deviceName = deviceNameFromId(chipIdStr);
 
   Serial.printf("Chip ID: %s | Device Name: %s\n",
